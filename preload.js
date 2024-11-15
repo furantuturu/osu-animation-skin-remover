e4eef82f85e7a8bf1e00ceba2c4f9f1057510cfe
Haemoffy <fuwansuu@gmail.com>
2024-11-15 23:27:11 +0800

    renamed ASI to ASE

--- preload.js
+++ preload.js
@@ -2,6 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('myAPI', {
     selectSkinDir: () => ipcRenderer.invoke('selectSkinDir'),
-    retrieveASIFiles: (skinDirPath) => ipcRenderer.invoke('retrieveASIFiles', skinDirPath),
-    asiFileDeletion: (skinDirPath, hitStdASI, nonGroupedASI) => ipcRenderer.invoke('ASIFileDeletion', skinDirPath, hitStdASI, nonGroupedASI)
+    retrieveASEFiles: (skinDirPath) => ipcRenderer.invoke('retrieveASEFiles', skinDirPath),
+    ASEFileDeletion: (skinDirPath, hitStdASE, nonGroupedASE) => ipcRenderer.invoke('ASEFileDeletion', skinDirPath, hitStdASE, nonGroupedASE)
 })
